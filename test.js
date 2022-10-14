605cacc15f54501d9493e70cc19ef69b3d6e38a3
Glory Amuah <glory.amuah@flutterwavego.com>
2022-10-14 01:01:31 +0100

    test: add test for strong password

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 const passwordStrength = require('./password-strength');
 
 
-//console.log(passwordStrength(' '));
-//console.log(passwordStrength('Wave'));
-//console.log(passwordStrength('Encyclopedias'));
+
+// console.log(passwordStrength('Wave'));
+// console.log(passwordStrength('Encyclopedias'));
 // console.log(passwordStrength('wavers'));
 // console.log(passwordStrength('Wavers'));
 // console.log(passwordStrength('@flutterWaVe'));
-// console.log(passwordStrength('@fluTTERw33!'));
+console.log(passwordStrength('@fluTTERw33!'));
 
